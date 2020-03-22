ff86972fb7d0e0b995b92acdbca9b7dca22d8ba1
Alexandre Campos <alexandrecamposmon@hotmail.com>
2020-03-22 13:59:20 -0300

    api meta controller

---
+++ ApiMetas/Controllers/MetasController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ApiMetas.Model;
+
+namespace ApiMetas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MetasController : ControllerBase
+    {
+        private readonly ApiDbContext _context;
+
+        public MetasController(ApiDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Metas
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Meta>>> GetMetas()
+        {
+            return await _context.Metas.ToListAsync();
+        }
+
+        // GET: api/Metas/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Meta>> GetMeta(Guid id)
+        {
+            var meta = await _context.Metas.FindAsync(id);
+
+            if (meta == null)
+            {
+                return NotFound();
+            }
+
+            return meta;
+        }
+
+        // PUT: api/Metas/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMeta(Guid id, Meta meta)
+        {
+            if (id != meta.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(meta).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MetaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Metas
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Meta>> PostMeta(Meta meta)
+        {
+            _context.Metas.Add(meta);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetMeta", new { id = meta.Id }, meta);
+        }
+
+        // DELETE: api/Metas/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Meta>> DeleteMeta(Guid id)
+        {
+            var meta = await _context.Metas.FindAsync(id);
+            if (meta == null)
+            {
+                return NotFound();
+            }
+
+            _context.Metas.Remove(meta);
+            await _context.SaveChangesAsync();
+
+            return meta;
+        }
+
+        private bool MetaExists(Guid id)
+        {
+            return _context.Metas.Any(e => e.Id == id);
+        }
+    }
+}
